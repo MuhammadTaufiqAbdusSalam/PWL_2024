8497ffdcaba8843e060d6424b63d84e8854a9929
MuhammadTaufiqAbdusSalam <arekmoker65@gmail.com>
2024-08-29 13:21:36 +0700

    TUGAS BIODATA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,71 @@
-<html lang="({ str_replace('_','-',app()->getlocale())})">
-    <head>
-        <meta charset ="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lanjut</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Biodata Muhammad Taufiq Abdus Salam</title>
+</head>
+
+<body>
+    <h1 align="center">PROVINSI JAWA TIMUR</h1>
+    <h1 align="center">KABUPATEN MOJOKERTO</h1>
+    <hr width="610px">
+    <table align="center" cellpadding="7px" width="620px">
+        <tr>
+            <td>NIK</td>
+            <td>: 351xxxxxxxxxxxx</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>: MUHAMMAD TAUFIQ ABDUS SALAM</td>
+        </tr>
+        <tr>
+            <td>Tempat / Tgl Lahir</td>
+            <td>: MOJOKERTO, xx-xx-2004</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>: LAKI-LAKI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gol Darah: -</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: DSN. PEKINGAN</td>
+        </tr>
+        <tr>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RT / RW </td>
+            <td>: 001 / 001</td>
+        </tr>
+        <tr>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kel/Desa </td>
+            <td>: SUMBERSONO</td>
+        </tr>
+        <tr>
+            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kecamatan </td>
+            <td>: DLANGGU</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>: ISLAM</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>: BELUM KAWIN</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>: PELAJAR/MAHASISWA</td>
+        </tr>
+        <tr>
+            <td>Kewarganegaraan</td>
+            <td>: WNI</td>
+        </tr>
+        <tr>
+            <td>Berlaku Hingga</td>
+            <td>: SEUMUR HIDUP</td>
+        </tr>
+    </table>
+</body>
+
 </html>
